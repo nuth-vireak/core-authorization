8bd51ddbea0db097a593e5d6d94bd5972cd4894b
PC12 <PC12@169.254.22.38>
2021-12-26 12:05:18 +0700

    update

---
+++ src/main/java/com/core/authorization/CoreAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.core.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreAuthorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/core/authorization/CoreAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
