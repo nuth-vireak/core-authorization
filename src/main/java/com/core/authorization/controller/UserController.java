cbd2c861718eccd459d90cc936ff20e23dfd4c93
PC12 <PC12@169.254.22.38>
2021-12-28 22:22:02 +0700

    update

--- src/main/java/com/core/authorization/controller/UserController.java
+++ src/main/java/com/core/authorization/controller/UserController.java
@@ -2,11 +2,13 @@ package com.core.authorization.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/doctor")
+@PreAuthorize("#oauth2.hasScope('read') or #oauth2.hasScope('write')")
 public class UserController {
 	
 	private Logger logger = LoggerFactory.getLogger(UserController.class);;
