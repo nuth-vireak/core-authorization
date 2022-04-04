d0d80add91ec9cd6db8f678f20b2bd8f2cc6ba44
somnanghoem <hoemsomnangruppa4@gmail.com>
2022-04-04 11:54:28 +0700

    testing

--- src/main/java/com/core/authorization/constant/ControllerName.java
+++ src/main/java/com/core/authorization/constant/ControllerName.java
@@ -3,7 +3,6 @@ package com.core.authorization.constant;
 public final class ControllerName {
 
 	private ControllerName() {}
-	
 	// User Controller
 	public static final String RETRIEVE_USER_LOGIN ="/USER1011";
 	public static final String REGISTER_USER_INFO  = "/USER1021";
