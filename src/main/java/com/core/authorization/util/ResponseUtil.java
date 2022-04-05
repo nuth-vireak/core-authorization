65f29471d6abf48cbabe85d50db0d10fc02274d1
somnanghoem <hoemsomnangruppa4@gmail.com>
2022-04-05 11:08:32 +0700

    add reponse GData

--- src/main/java/com/core/authorization/util/ResponseUtil.java
+++ src/main/java/com/core/authorization/util/ResponseUtil.java
@@ -1,11 +1,95 @@
 package com.core.authorization.util;
 
+import org.apache.commons.lang3.StringUtils;
+
 import jara.platform.collection.GData;
+import jara.platform.collection.GListData;
 
 public class ResponseUtil {
 
 	private static final String CONTROLLER_NAME = "CONTROLLER_NAME";
 
+	
+	/**
+	 * -- detail description --
+	 *
+	 * @serviceID 
+	 * @logicalName 
+	 * @param gData
+	 * @param param
+	 * @return
+	 * @exception 
+	 * @fullPath 
+	 */
+	public static GData toGData( GData gData, String... param ) {
+		GData outputData = new GData();
+		for ( String key: param ) {
+			if ( gData.containsKey(key) ) {
+				outputData.set(key, gData.get(key) );
+			} else {
+				outputData.set(key, StringUtils.EMPTY );
+			}
+		}
+		return outputData;
+	}
+	
+	/**
+	 * -- detail description --
+	 *
+	 * @serviceID 
+	 * @logicalName 
+	 * @param listName
+	 * @param mMultiData
+	 * @param param
+	 * @return
+	 * @exception 
+	 * @fullPath 
+	 */
+	public static GData toMData( String listName, GListData mMultiData, String... param ) {
+		return toGDataWithListName( mMultiData, listName, param );
+	}
+	
+	/**
+	 * -- detail description --
+	 *
+	 * @serviceID 
+	 * @logicalName 
+	 * @param mMultiData
+	 * @param listName
+	 * @param param
+	 * @return
+	 * @exception 
+	 * @fullPath 
+	 */
+	private static GData toGDataWithListName( GListData mMultiData, String listName, String... param ) {
+		GData outputData = new GData();
+		
+		GListData grid = new GListData();
+		for ( GData GData : mMultiData.toListGData() ) {
+			GData cloneGData = new GData();
+			for ( String key : param ) {
+				if ( GData.containsKey( key ) ) {
+					cloneGData.set( key, GData.get( key ) );
+				} else {
+					cloneGData.set( key, StringUtils.EMPTY );
+				}
+			}
+			grid.add( cloneGData );
+		}
+		outputData.set( String.format( "%s", listName ), grid );
+		
+		return outputData;
+	}
+	/**
+	 * -- detail description --
+	 *
+	 * @serviceID 
+	 * @logicalName 
+	 * @param controllerName
+	 * @throws Exception
+	 * @exception 
+	 * @fullPath 
+	 */
 	public static void setControllerName(GData controllerName) throws Exception {
 
 		GData mMultiData = getControllerName();
@@ -14,6 +98,15 @@ public class ResponseUtil {
 		ContextHolder.setContextObject(	CONTROLLER_NAME, mMultiData	);
 	}
 
+	/**
+	 * -- detail description --
+	 *
+	 * @serviceID 
+	 * @logicalName 
+	 * @return
+	 * @exception 
+	 * @fullPath 
+	 */
 	public static GData getControllerName() {
 
 		GData mMultiData = ( GData ) ContextHolder.getContextObject(	CONTROLLER_NAME	);
@@ -23,6 +116,14 @@ public class ResponseUtil {
 		return mMultiData;
 	}
 
+	/**
+	 * -- detail description --
+	 *
+	 * @serviceID 
+	 * @logicalName 
+	 * @exception 
+	 * @fullPath 
+	 */
 	public static void clearControllerName() {
 
 		ContextHolder.remove(	CONTROLLER_NAME	);
